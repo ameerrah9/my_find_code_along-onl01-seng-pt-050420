c44e8ecd100f6ac72e92118bfb45beff1cf5fffc
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-05-13 09:55:01 -0400

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+    while i < collection.length
+    #if the block returns true
+        return collection[i] if yield(collection[i]) #return the element in the array that was true    
+        i = i + 1
+    end
 end
+
+#method will return the first element in the array that evaluates to true. 
+#In our example this would be 15.
